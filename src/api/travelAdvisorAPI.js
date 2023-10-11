4c048d195d2488323696d3abd04a38a9f1ea8980
Shashikar Saurabh <shashikarsaurabh@gmail.com>
2023-10-11 23:06:11 +0530

    Initial Commit

---
+++ src/api/travelAdvisorAPI.js
@@ -0,0 +1,49 @@
+/* eslint-disable consistent-return */
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
+        headers: {
+          "x-rapidapi-key": process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY || "c7b95762edmsh433ff52ddb7953fp1b58f1jsne1f054f5fcab",
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get(
+        "https://community-open-weather-map.p.rapidapi.com/find",
+        {
+          params: { lat, lon: lng },
+          headers: {
+            "x-rapidapi-key": process.env.REACT_APP_RAPID_API_WEATHER_API_KEY || "c7b95762edmsh433ff52ddb7953fp1b58f1jsne1f054f5fcab",
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          },
+        }
+      );
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
